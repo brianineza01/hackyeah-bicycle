a5104ef554852e5c8d7ea9dca78ec680763b1ce9
Kevin Kimenyi <kimenyikevin@gmail.com>
2024-09-28 19:07:40 +0200

    get it done

--- src/app/api/incidents/route.ts
+++ src/app/api/incidents/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import qs from 'qs';
+import qs from "qs";
 
 /**
  * 1	These roads are meant for high volume, maximum speed traffic between and through major metropolitan areas. There are very few, if any, speed changes. Access to this road is usually controlled.
@@ -7,13 +7,13 @@ import qs from 'qs';
  * 3	These roads interconnect with First Class Roads (FRC2) and provide a high volume of traffic movement at a lower level of mobility than First Class Roads (FRC2).
  * 4	These roads provide for a high volume of traffic movement at moderate speeds between neighborhoods. These roads connect with higher Functional Class roads to collect and distribute traffic between neighborhoods.
  * 5	These roads' volume and traffic movements are below the level of any other road.
- * @param request 
- * @returns 
+ * @param request
+ * @returns
  */
 
-export const GET = async (request: Request ) => {
-    const rawParams = request.url.split('?')[1];
-    const params = qs.parse(rawParams);
+export const GET = async (request: Request) => {
+  const rawParams = request.url.split("?")[1];
+  const params = qs.parse(rawParams);
   console.log(params);
   const response = await fetch(
     `https://data.traffic.hereapi.com/v7/incidents?in=circle:${params.long},${params.lat};r=${params.r}&locationReferencing=olr&functionalClasses=2,3,4,5`,
@@ -21,8 +21,8 @@ export const GET = async (request: Request ) => {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.HERE_TOKEN_KEY}`
-      }
+        Authorization: `Bearer ${process.env.HERE_TOKEN_KEY}`,
+      },
     }
   );
 
