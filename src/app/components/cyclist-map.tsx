c523dd1a0ffdf9c7b44627fb1af2782359a282fb
B3Kay <benjaminjekarlsson@gmail.com>
2024-09-28 19:41:39 +0200

    Add map

---
+++ src/app/components/cyclist-map.tsx
@@ -0,0 +1,25 @@
+'use client'
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import "leaflet/dist/leaflet.css";
+
+const position = [51.505, -0.09]
+
+export function CyclistMap() {
+
+    return (
+
+
+        <MapContainer className="w-full h-full bg-slate-800 absolute" center={position} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+        // </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,24 @@ import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import { SelectAdress } from "./components/select-adress";
 import { Label } from "@/components/ui/label";
+import { CyclistMap } from "./components/cyclist-map";
 
 export default function Home() {
   return (
     <div>
       {/* Map */}
-      <div className="w-full h-full bg-slate-800 absolute">
-
+      <div className="w-full h-full bg-slate-800 absolute z-10">
+        <CyclistMap />
       </div>
       {/* upper controller / input */}
-      <div className="absolute top-0 left-0 right-0 bg-white shadow-md rounded-md p-4 m-4">
+      <div className="z-20 absolute top-0 left-0 right-0 bg-white shadow-md rounded-md p-4 m-4">
         <SelectAdress></SelectAdress>
         <SelectAdress></SelectAdress>
       </div>
 
 
       {/* lover controller  */}
-      <div className="absolute bottom-0 left-0 right-0 bg-white shadow-md rounded-md p-4 m-4">
+      <div className="z-30 absolute bottom-0 left-0 right-0 bg-white shadow-md rounded-md p-4 m-4">
         Her we have some stfuff
       </div>
 
