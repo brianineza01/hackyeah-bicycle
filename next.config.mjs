5c49b9c478f165ab159e13a6d89040f49082c55f
Brian Ineza <brianineza01@gmail.com>
2024-09-29 11:45:11 +0200

    ft: prod release

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
